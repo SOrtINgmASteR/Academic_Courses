7f11cec4074cb1f346b0cf84872a9cef1bb1feed
Noor Mohammed Priom <noor105464@gmail.com>
2026-05-16 11:00:23 +0600

    Lab Work 5 Done

--- CSE 430 Compiler Design Lab/Lab Work - 5/three_address_code.py
+++ CSE 430 Compiler Design Lab/Lab Work - 5/three_address_code.py
@@ -22,48 +22,34 @@ def infix_to_postfix(expression):
 
     i = 0
     while i < len(expression):
-
         char = expression[i]
 
-        # Ignore spaces
         if char == ' ':
             i += 1
             continue
 
-        # Operand
-        if char.isalnum():
+        elif char.isalnum():
             operand = ""
-
             while i < len(expression) and expression[i].isalnum():
                 operand += expression[i]
                 i += 1
-
             postfix.append(operand)
             continue
 
-        # Left Parenthesis
         elif char == '(':
             stack.append(char)
 
-        # Right Parenthesis
         elif char == ')':
             while stack and stack[-1] != '(':
                 postfix.append(stack.pop())
             stack.pop()
 
-        # Unary minus
         elif char == '-' and (i == 0 or expression[i - 1] in "(+-*/%^"):
             stack.append('u-')
 
-        # Operator
         else:
-            while (
-                stack and
-                stack[-1] != '(' and
-                precedence(stack[-1]) >= precedence(char)
-            ):
+            while (stack and stack[-1] != '(' and precedence(stack[-1]) >= precedence(char)):
                 postfix.append(stack.pop())
-
             stack.append(char)
 
         i += 1
@@ -77,60 +63,51 @@ def infix_to_postfix(expression):
 # Generate TAC from postfix
 def generate_TAC(postfix):
     global temp_count
-
     stack = []
     tac = []
 
     for token in postfix:
 
-        # Unary minus
         if token == 'u-':
             operand = stack.pop()
-
             temp = f"T{temp_count}"
             temp_count += 1
-
             tac.append(f"{temp} = uminus {operand}")
             stack.append(temp)
 
-        # Binary operator
         elif token in operators:
-
             right = stack.pop()
             left = stack.pop()
-
             temp = f"T{temp_count}"
             temp_count += 1
-
             tac.append(f"{temp} = {left} {token} {right}")
-
             stack.append(temp)
 
-        # Operand
         else:
             stack.append(token)
 
     return tac
 
 
-# MAIN PROGRAM
-
-# Read expression from file
+# Read expressions line by line from file
 with open("input.txt", "r") as file:
-    expression = file.read().strip()
+    for expression in file:
+        expression = expression.strip()
+        if not expression:
+            continue
+        print("================================")
+        print("Input Expression:")
+        print(expression)
 
-print("Input Expression:")
-print(expression)
+        # Convert infix to postfix
+        postfix = infix_to_postfix(expression)
+        print("\nPostfix Expression:")
+        print(" ".join(postfix))
 
-# Convert to postfix
-postfix = infix_to_postfix(expression)
+        # Generate TAC
+        tac_code = generate_TAC(postfix)
+        print("\nThree Address Code:")
+        for line in tac_code:
+            print(line)
 
-print("\nPostfix Expression:")
-print(" ".join(postfix))
-
-# Generate TAC
-tac_code = generate_TAC(postfix)
-
-print("\nThree Address Code:")
-for line in tac_code:
-    print(line)
+        print("================================\n")
